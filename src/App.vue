2209efdab67c9e46b84a0c241fa99777f9e9a366
mr.wick2 <jannwildelgado@gmail.com>
2021-07-15 13:58:46 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,114 @@
+/* eslint-disable vue/no-parsing-error */
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container fill-height>
+        <div class="page">
+          <v-card
+            elevation="0"
+            outlined
+            class="max-auto"
+          >
+            <v-card-text>
+              <v-btn
+                v-for="(sound, key) of soundList"
+                :key="key"
+                class="ma-2"
+                :class="[
+                  {'on-active':currPlaying === sound}
+                ]"
+                color="secondary"
+                elevation="2"
+                @click="initSounds(sound)"
+                :dark="currPlaying === sound"
+              >
+                <v-icon
+                  v-if="currPlaying === sound"
+                  class="mr-2"
+                >
+                  mdi-volume-high
+                </v-icon>
+
+                {{ sound }}
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </div>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import { Howl } from 'howler'
 
 export default {
-  name: 'App',
+  name: 'SoundPad',
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
-    //
+    id: null,
+    sound: null,
+    currPlaying: '',
+    soundList: [
+      'sound1',
+      'sound2',
+      'sound3',
+      'sound4',
+      'sound5',
+      'sound6',
+      'sound7',
+      'sound8',
+      'sound9'
+    ]
   }),
+
+  created () {},
+
+  mounted () {
+    document.title = 'SoundPad'
+  },
+
+  methods: {
+    initSounds (sound) {
+      if (this.id) {
+        this.sound.stop(this.id)
+      }
+
+      const path = require(`./assets/sounds/${sound}.mp3`)
+
+      this.sound = new Howl({
+        src: [path],
+        autoplay: false,
+        loop: false,
+        volume: 1,
+        onplay: () => {
+          this.currPlaying = sound
+          console.log('Now playing...')
+        },
+        onend: () => {
+          this.currPlaying = ''
+          console.log('Finished!')
+        }
+      })
+
+      this.id = this.sound.play()
+    }
+  }
 };
 </script>
+
+<style lang="css" scoped>
+.page {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.on-active {
+  background-image: linear-gradient(to right, #84fab0 0%, #8fd3f4 51%, #84fab0 100%);
+}
+</style>
